3e27a287bd1f65eb503e648823a04f363bcdd735
Serim <jkaae93@gmail.com>
2021-04-01 16:45:44 +0900

    Add error handling

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# KisconScript
-For Tampermonkey
+# KISCON Script for Tampermonkey Getting Started
+
+## Installation
+- [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)
+
+## Setting
+1. ```get_kiscon.js``` 코드 복사
+2. Tampermonkey -> 새 스크립트 만들기...
+3. 코드 붙여넣기
+4. KISCON에서 검색할 정보 설정 (업종 , 지역등 ex. 토목공사업 검색시 토목공사업 체크)
+5. 스크립트 활성화
+5. KISCON에서 중간에 검색 클릭
+6. 개발자모드 실행 (F12)
+7. 로그 출력 확인
+
+## Note
+- ```https://www.kiscon.net/gongsi/step_custom.asp``` 에서만 동작
+- UI가 변경되면 동작하지 않을 수 있음
+- 사이트 버그로 인해 회사명과 대표자 명만 추가될 수 있음 (이 경우 수동으로 입력해야함)
+- 간혹 타이밍때문에 회사명과 대표자명만 반복 추가될 수 있음 (삭제하면 됨)
--- get_kiscon.js
+++ get_kiscon.js
@@ -10,57 +10,126 @@
 // @grant        none
 // ==/UserScript==
 
+var nameList = [];
+var ceoList = [];
+var putList = [];
+
 (function() {
-   var indexSize = $('tbody').find('.lnone').length -1;
-   $('#simple_info_c').click(function() {
-       var companyName = $('#strSangho').text().replace(/,/g,' ');
-       var ceoName = $('#strCeo').text().replace(/,/g,' ');
-       var tel = $('#strTel').text().replace(/,/g,' ');
-       var addr = $('#strAddr').text().replace(/,/g,' ');
-       var type = $('#strItem').text().replace(/,/g,' ');
-       var data = localStorage.getItem("0");
-
-       if(data == null) data = [];
-       else data = JSON.parse(data);
-       console.log(data);
-       var index = $('#strItem').text().split("]").indexOf("[철근ㆍ콘크리트공사업");
-       var state = $('#strItem').text().split("]")[index+2].replace('[','');
-       data.push([companyName,ceoName,tel,addr,state,type]);
-       localStorage.setItem("0", JSON.stringify(data));
-       $('.modal_close')[2].click();
-    });
-
-
-    $('.btn-in.btncol_yo').each(function(index) {
-        $(this).click(function() {
-            if(indexSize == index) {
-                if($('.btn_prevNext').find('a')[1].getAttribute("style") != null) { // next page check
-                    console.log("last page");
-                    if(window.confirm("Script is done. download list?")){
-                        downloadCsv(data);
-                    } else {
-                        console.log("can't download");
-                    }
-                } else {
-                    $('.btn_prevNext').find('a')[1].click();
-                }
-            };
-        });
-        $(this).click();
-   });
-
-    function downloadCsv(data) {
-        var lineArray = [];
-        data.forEach(function (infoArray, index) {
-            var line = infoArray.join(",");
-            lineArray.push(index == 0 ? "상호, 대표자, 전화번호, 주소, 업종상태\n" + line : line);
-        });
-        var csvContent = lineArray.join("\n");
-        var pom = document.createElement('a');
-        var blob = new Blob(["\ufeff"+csvContent], {type:'text/csv;charset=utf-8'});
-        var url = URL.createObjectURL(blob);
-        pom.href = url;
-        pom.setAttribute('download', 'kiscon_list.csv');
-        pom.click();
-    }
+    // main
+    $('.paginationz_wrap.visible-xs').empty(); // delete mobile page
+    getNameList();
+    setClickEvent();
+    clickSimpleInfo();
+    console.log('start timeout');
+    setTimeout(function() {
+        console.log('stopped timeout');
+        errHandle();
+        $('.btn_prevNext').find('a')[1].click();
+    },20000);
 })();
+
+function getNameList() {
+    var list = [];
+    $('.basic_tbl.thickgr.w100.mgtp08.flft').find('tr td').each(function(index) {
+        $(this).each(function(index) {
+            list.push(this.innerText);
+        });
+    });
+    list.forEach(function(value, index) {
+        switch(index) {
+            case 1: case 10: case 19: case 28: case 37:
+                nameList.push(list[index]);
+                break;
+            case 3: case 12: case 21: case 30: case 39:
+                ceoList.push(list[index]);
+                break;
+        }
+    });
+    console.log(nameList);
+    console.log(ceoList);
+}
+
+function setClickEvent() {
+    $('#simple_info_c').click(function() {
+
+        var companyName = $('#strSangho').text().replace(/,/g,' ');
+        var ceoName = $('#strCeo').text().replace(/,/g,' ');
+        var tel = $('#strTel').text().replace(/,/g,' ');
+        var addr = $('#strAddr').text().replace(/,/g,' ');
+        var type = $('#strItem').text().replace(/,/g,' ');
+        var data = localStorage.getItem("0");
+
+        if(data == null) data = [];
+        else data = JSON.parse(data);
+        console.log('name ' + companyName);
+        var index = $('#strItem').text().split("]").indexOf("[철근ㆍ콘크리트공사업");
+        var state = $('#strItem').text().split("]")[index+2].replace('[','');
+        data.push([companyName,ceoName,tel,addr,state,type]);
+        localStorage.setItem("0", JSON.stringify(data));
+
+        $('.modal_close')[2].click();
+        putList.push(companyName);
+        saveRequest();
+    });
+}
+
+function saveRequest() {
+    var data = localStorage.getItem("0");
+    data = JSON.parse(data);
+    var size = Number($('.org01.dml').text().replace(',',''));
+    console.log("size: " + size + " current: " + data.length);
+    if(data.length >= size-1) {
+        if($('.btn_prevNext').find('a')[1].getAttribute("style") != null) { // next page check
+            console.log("last page");
+            if(window.confirm("Script is done. download list?")){
+                downloadCsv(data);
+            } else {
+                console.log("can't download");
+            }
+        }
+    } else {
+        var expect = Number($('.active').text()) * 5;
+        if(data.length == expect) {
+            $('.btn_prevNext').find('a')[1].click();
+        } else {
+            console.warn('please check data. current data is ' + data.length + ' expect data is ' + expect);
+        }
+    };
+}
+
+function downloadCsv(data) {
+    var lineArray = [];
+    data.forEach(function (infoArray, index) {
+        var line = infoArray.join(",");
+        lineArray.push(index == 0 ? "상호, 대표자, 전화번호, 업종상태, 주소\n" + line : line);
+    });
+    var csvContent = lineArray.join("\n");
+    var pom = document.createElement('a');
+    var blob = new Blob(["\ufeff"+csvContent], {type:'text/csv;charset=utf-8'});
+    var url = URL.createObjectURL(blob);
+    pom.href = url;
+    pom.setAttribute('download', 'kiscon_list.csv');
+    pom.click();
+}
+
+function clickSimpleInfo() {
+    $('.btn-in.btncol_yo').each(function(index) {
+        $(this).click();
+    });
+}
+
+function errHandle() {
+    nameList.forEach(function(value, index) {
+        if(value != putList[index]) {
+            console.log(value);
+            var data = localStorage.getItem('0');
+            var companyName = value;
+            var ceoName = ceoList[index];
+            if(data == null) data = [];
+            else data = JSON.parse(data);
+            console.log('error company name ' + companyName + ' ceo: ' + ceoName);
+            data.push([companyName,ceoName,'','','','']);
+            localStorage.setItem('0', JSON.stringify(data));
+        }
+    });
+}
